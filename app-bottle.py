c8c578983e3b749f98c4c921d4acbfd74c9695d1
Martin Ruskov <martin.ruskov@gmail.com>
2020-05-16 07:55:40 +0200

    Added web server

---
+++ app-bottle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import os
+
+from datetime import datetime
+from urllib.parse import quote
+
+from bottle import Bottle
+from bottle import request, redirect, abort, static_file
+
+from settings import allowed_extensions, allowed_mime_types
+from settings import upload_folder, max_content_length, static_path
+
+app = Bottle(__name__)
+
+
+def allowed_file(filename: str, file_mime_type: str) -> bool:
+    return (
+        "." in filename
+        and filename.rsplit(".", 1)[1].lower() in allowed_extensions
+        and file_mime_type in allowed_mime_types
+    )
+
+
+@app.post("/")
+def upload():
+    """Handle file upload form"""
+
+    # get the 'newfile' field from the form
+    try:
+        newfile = request.files.get("fileupload")
+    except:
+        return redirect("/?" + quote("Неочаквана грешка"))
+
+    if not allowed_file(newfile.filename, newfile.content_type):
+        return redirect("/?" + quote("Файлът не може да бъде разчетен"))
+
+    if not os.path.exists(upload_folder):
+        os.makedirs(upload_folder)
+    timestamped = datetime.now().strftime("%Y%m%d%H%M%S") + newfile.filename
+    save_path = os.path.join(upload_folder, timestamped)
+    newfile.save(save_path)
+
+    # redirect to home page if it all works ok
+    return redirect("/?" + quote("Файлът е качен!"))
+
+
+@app.get("/")
+def root():
+    return static_file("index.html", root=static_path)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, reloader=True)
---
+++ settings.py
@@ -0,0 +1,20 @@
+upload_folder = "uploads"
+
+allowed_uploads = {
+    "doc": "application/msword",
+    "docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "odt": "application/vnd.oasis.opendocument.text",
+}
+allowed_extensions = allowed_uploads.keys()
+max_content_length = 16 * 1024 * 1024
+
+# flask-uploads
+UPLOADS_DEFAULT_DEST = upload_folder + "/project/static/img/"
+UPLOADS_DEFAULT_URL = "http://localhost:5000/static/img/"
+
+UPLOADED_IMAGES_DEST = upload_folder + "/project/static/img/"
+UPLOADED_IMAGES_URL = "http://localhost:5000/static/img/"
+
+allowed_mime_types = allowed_uploads.values()
+
+static_path = "static"
