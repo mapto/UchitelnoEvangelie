e79313c7f42cf67d3a7dcb4de649876c2cfa2c30
Martin Ruskov <martin.ruskov@gmail.com>
2021-01-23 04:32:58 +0100

    ordered alphabets

--- validate.sh
+++ validate.sh
@@ -2,12 +2,16 @@
 
 echo '>>> Skipping Pylint'
 # pylint -E extractor
+# pylint -E integrator/
 
 echo '>>> Running Mypy'
 mypy extractor
+mypy integrator
 
 echo '>>> Running Pytest'
 pytest -vv --doctest-modules -s extractor # --disable-warnings
+pytest -vv --doctest-modules -s integrator # --disable-warnings
 
 echo '>>> Running Black'
 black extractor
+black integrator
