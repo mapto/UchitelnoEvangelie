5be62e473d9a20f51f75629e6a405b8ea0ba294e
mapto <martin.ruskov@gmail.com>
2021-05-26 11:15:12 +0200

    Expanded blackbox test on extractor

--- test.sh
+++ test.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 cd extractor/test
-for filename in *.docx; do
-    ../extractor.py -p $filename
-    stripped="${filename%.docx}"
+PARTS="docProps/app.xml xl/theme/theme1.xml xl/worksheets/sheet1.xml xl/styles.xml _rels/.rels xl/workbook.xml xl/_rels/workbook.xml.rels [Content_Types].xml"
+# SKIPPED: docProps/core.xml, as it contains no relevant information, but timestamps
+for FILENAME in *.docx; do
+    ../extractor.py -p $FILENAME
+    stripped="${FILENAME%.docx}"
     unzip "$stripped.expected.xlsx" -d expected
     unzip "$stripped.xlsx" -d out
-    if [ -z "$(diff expected/xl/worksheets/sheet1.xml out/xl/worksheets/sheet1.xml)" ]; then
-        echo "No differences"
-    else
-        echo "DIFFERENCES FOUND!"
-    fi
+    for NEXT in $PARTS; do 
+        if [ -z "$(diff expected/$NEXT out/$NEXT)" ]; then
+            echo "No differences in $NEXT"
+        else
+            echo "DIFFERENCES FOUND in $NEXT!"
+            exit 0
+        fi
+    done
     rm -rf expected
     rm -rf out
     rm "$stripped.xlsx"
