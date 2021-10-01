528d80249f69cee7763d035d1967d088f7fe15a3
mapto <martin.ruskov@gmail.com>
2021-10-01 21:01:30 +0200

    Merged tyam_li branch

--- integrator/merger.py
+++ integrator/merger.py
@@ -2,18 +2,50 @@
 
 """A processor merging multiple lines when they are related"""
 
-from typing import List
+from typing import Dict, List
 
 from const import IDX_COL, MISSING_CH, STYLE_COL, V_LEMMA_SEP
 from model import Index
-from semantics import LangSemantics, MainLangSemantics
+from semantics import LangSemantics, MainLangSemantics, VarLangSemantics
 from util import ord_word
 
 ord_tuple = lambda x: ord_word(x[0])
 
 
+def _group_variants(group: List[List[str]], sem: MainLangSemantics) -> str:
+    """Returns a list of variants (excluding main) that are present in this group"""
+    variants = set()
+    for row in group:
+        for var in [k for k, val in sem.var.multiword(row).items() if val]:
+            variants.add(var)
+    return "".join(variants).strip()
+
+
 def _collect(group: List[List[str]], col: int) -> List[str]:
+    """Collects the actual content in the group column"""
     return [group[i][col] for i in range(len(group)) if group[i][col]]
+
+def _normalise_multiword(multiword: Dict[str, str]) -> Dict[str, str]:
+    result = {}
+    for k, v in multiword.items():
+        # split keys in single characters
+        for c in k:
+            result[c] = v
+    return result
+
+def _collect_multiword(group: List[List[str]], sem: MainLangSemantics) -> str:
+    """Collects the content of the multiwords for a variant in a group into a single string.
+    The output is conformant with the multiword syntax.
+    Yet it might contain redundancies, due to the normalisation process (split of equal variants)"""
+    collected : Dict[str, str]= {}
+    for row in group:
+        # for k, v in _normalise_multiword(sem.var.multiword(row)).items():
+        for k, v in sem.var.multiword(row).items():
+            if k in collected:
+                collected[k] = collected[k] + " " + v
+            else:
+                collected[k] = v
+    return " ".join([f"{v} {k}" for k, v in collected.items() if v.strip()])
 
 
 def _highlighted(group: List[List[str]], col: int) -> bool:
@@ -52,9 +84,12 @@ def _merge_indices(group: List[List[str]]) -> Index:
 
 
 def _close(
-    group: List[List[str]], orig: LangSemantics, trans: LangSemantics
+    group: List[List[str]], orig: MainLangSemantics, trans: MainLangSemantics
 ) -> List[List[str]]:
-    """*IN_PLACE*"""
+    """Wraps up a group that is currently being read.
+    *IN_PLACE*
+    Redistributes content according to desired (complex) logic
+    """
     if not group:
         return []
     if not group[0][IDX_COL]:
@@ -75,10 +110,23 @@ def _close(
 
     idx = _merge_indices(group)
 
+    # populate variants equal to main
+    variants = _group_variants(group, orig)
+    if variants:
+        for row in group:
+            print("*" * 10)
+            print(row)
+            if row[orig.word] and not row[orig.var.word]:
+                row[orig.var.word] = f"{row[orig.word]} {variants}"
+            if row[orig.lemmas[0]] and not row[orig.var.lemmas[0]]:
+                row[orig.var.lemmas[0]] = row[orig.lemmas[0]]
+
     # collect content
     line = [""] * STYLE_COL
-    for c in orig.word_cols() + trans.word_cols():
+    for c in [orig.word, trans.word]:
         line[c] = " ".join(_collect(group, c))
+    line[orig.var.word] = _collect_multiword(group, orig)
+    line[trans.var.word] = _collect_multiword(group, trans)
     for c in trans.lem1_cols():
         g = [e for e in _collect(group, c) if e.strip() != MISSING_CH]
         line[c] = f" {V_LEMMA_SEP} ".join(g)
@@ -125,8 +173,8 @@ def merge(
     for raw in corpus:
         row = [v if v else "" for v in raw]
 
-        # if row[IDX_COL] == "1/5a5":
-        # print(row)
+        if "1/6a10" in row[IDX_COL]:
+            print(row)
 
         not_blank = [v for v in row if v]
         if not row[IDX_COL] and not_blank:
