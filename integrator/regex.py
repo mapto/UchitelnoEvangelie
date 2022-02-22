6ea725961b0e9032fcf0f41ae6862ff63d2ce2f8
Martin Ruskov <martin.ruskov@gmail.com>
2022-02-22 16:44:05 +0100

    More generic sources

--- integrator/regex.py
+++ integrator/regex.py
@@ -3,6 +3,6 @@ idx_regex = (
     r"(\d{1,2})\/(W)?(\d{1,3})([a-d])(\d{1,2})(\{(\d)\})?(\{(\d)\})?"
     + r"(-((((\d{1,2})\/)?(W)?(\d{1,3}))?([a-d]))?(\d{1,2})(\{(\d)\})?(\{(\d)\})?)?"
 )
-source_regex = r"([A-Z][a-z]?)"
+source_regex = r"([A-Z]\w*)"
 multiword_regex = r"^([^A-Z]+)(" + source_regex + r"+)(.*)$"
 multilemma_regex = r"^([^A-Z\+]+)(\+\s\w+\.)?(" + source_regex + r"+)?(\s*[\&\/])?(.*)$"
