95ed6f636a7353a533d5e1fbc129fcdb9884ed1f
Martin Ruskov <martin.ruskov@gmail.com>
2022-04-27 14:52:28 +0200

    Fixed ordering of Index, Alternative, Usage

--- integrator/model.py
+++ integrator/model.py
@@ -219,7 +219,10 @@ class Source:
 class Alternative:
     """Word occurences in a line are counted.
     For main alternative this is in a separate variable,
-    but for variants, it's part of the dictionary"""
+    but for variants, it's part of the dictionary
+    >>> Alternative("аще", main_word="аще") < Alternative("\ue205 conj.", main_word="\ue205")
+    True
+    """
 
     main_lemma: str = ""
     var_lemmas: Dict[Source, str] = field(default_factory=lambda: {})
@@ -230,12 +233,64 @@ class Alternative:
     def __bool__(self) -> bool:
         return bool(self.main_lemma) or bool(self.var_lemmas)
 
+    def __lt__(self, other) -> bool:
+        if self.main_word < other.main_word:
+            return True
+        elif self.main_word > other.main_word:
+            return False
+
+        self_var_word_keys = str(Source(sum(self.var_words.keys())))
+        other_var_word_keys = str(Source(sum(other.var_words.keys())))
+        if self_var_word_keys < other_var_word_keys:
+            return True
+        elif self_var_word_keys > other_var_word_keys:
+            return False
+
+        self_var_word_values = "".join(v[0] for v in self.var_words.values())
+        other_var_word_values = "".join(v[0] for v in other.var_words.values())
+        if self_var_word_values < other_var_word_values:
+            return True
+        elif self_var_word_values > other_var_word_values:
+            return False
+
+        if self.main_cnt < other.main_cnt:
+            return True
+        elif self.main_cnt > other.main_cnt:
+            return False
+
+        return False
+
+    def __le__(self, other) -> bool:
+        return self < other or self == other
+
+    def __gt__(self, other) -> bool:
+        return not self <= other
+
+    def __ge__(self, other) -> bool:
+        return not self < other
+
 
 @dataclass(frozen=True)
 class Usage:
     """Variant is not only indicative, but also nominative - which variant.
     Here alt means other other transcriptions (main or var).
-    Contrast these to Index"""
+    Contrast these to Index
+
+    >>> i = Index(1, False, 7, "c", 6, word="om.")
+    >>> s = Source("WH")
+    >>> a = Usage(i, "gr", s, trans_alt=Alternative("аще", main_word="аще"))
+    >>> b = Usage(i, "gr", s, trans_alt=Alternative("\ue205 conj.", main_word="\ue205"))
+    >>> a < b
+    True
+
+    >>> i1 = Index(5, False, 22, "b", 5, word="оуслышат\ue205 GH")
+    >>> i2 = Index(5, False, 22, "b", 5, 2, 2, word="оуслышат\ue205 GH")
+    >>> s = Source("GH")
+    >>> a = Usage(i1, "sl", s, Alternative("слꙑшат\ue205", main_word="слꙑшат\ue205"))
+    >>> b = Usage(i2, "sl", s, Alternative("послꙑшат\ue205", main_word="послꙑшат\ue205"))
+    >>> a < b
+    True
+    """
 
     idx: Index
     lang: str
@@ -251,6 +306,13 @@ class Usage:
             self.idx < other.idx
             or self.idx == other.idx
             and len(self.var) < len(other.var)
+            or self.idx == other.idx
+            and len(self.var) == len(other.var)
+            and self.orig_alt < other.orig_alt
+            or self.idx == other.idx
+            and len(self.var) == len(other.var)
+            and self.orig_alt == other.orig_alt
+            and self.trans_alt < other.trans_alt
         )
 
     def __le__(self, other) -> bool:
