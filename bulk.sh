11a4cf933cf55652da7c19879a5881ec18f38218
Martin <martin.ruskov@gmail.com>
2023-09-07 21:50:07 +0200

    Commented buld script

--- bulk.sh
+++ bulk.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Call from the directory of the xlsx files.
+# This is in silent mode, but will still print warnings and errors.
+
 # from https://stackoverflow.com/a/246128/1827854
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo $SCRIPT_DIR
